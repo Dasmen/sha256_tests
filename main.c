62ee8d0ada2b1855057ada1079562312acf5f751
DeckerSU <support@decker.su>
2018-05-31 20:02:03 +0300

    vcalc_sha256 -> vcalc_sha256_hardware

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include "cryptoext/sha256.h"
 
 int32_t init_hexbytes_noT(char *hexbytes,uint8_t *message,long len);
-void vcalc_sha256(char hashstr[(256 >> 3) * 2 + 1],uint8_t hash[256 >> 3],uint8_t *src,int32_t len);
+void vcalc_sha256_hardware(char hashstr[(256 >> 3) * 2 + 1],uint8_t hash[256 >> 3],uint8_t *src,int32_t len);
 
 /* iguana_utils.c */
 
@@ -27,10 +27,12 @@ int32_t init_hexbytes_noT(char *hexbytes,unsigned char *message,long len)
         hexbytes[0] = 0;
         return(1);
     }
+
     for (i=0; i<len; i++)
     {
         hexbytes[i*2] = hexbyte((message[i]>>4) & 0xf);
         hexbytes[i*2 + 1] = hexbyte(message[i] & 0xf);
+        //snprintf(hexbytes+i*2, 3, "%02x", message[i] & 0xff);
         //printf("i.%d (%02x) [%c%c]\n",i,message[i],hexbytes[i*2],hexbytes[i*2+1]);
     }
     hexbytes[len*2] = 0;
@@ -39,10 +41,16 @@ int32_t init_hexbytes_noT(char *hexbytes,unsigned char *message,long len)
 }
 
 // vcalc_sha256 with hardware acceleration
-void vcalc_sha256(char hashstr[(256 >> 3) * 2 + 1],uint8_t hash[256 >> 3],uint8_t *src,int32_t len) {
+void vcalc_sha256_hardware(char hashstr[(256 >> 3) * 2 + 1],uint8_t hash[256 >> 3],uint8_t *src,int32_t len) {
     SHA256_hash(src, len, hash);
      if ( hashstr != 0 )
         init_hexbytes_noT(hashstr,hash,256 >> 3);
+
+    /*
+    snprintf(hashstr, (256 >> 3) * 2 + 1, "%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x", hash[0],
+             hash[1],hash[2],hash[3],hash[4],hash[5],hash[6],hash[7],hash[8],hash[9],hash[10],hash[11],hash[12],hash[13],hash[14],hash[15],
+             hash[16],hash[17],hash[18],hash[19],hash[20],hash[21],hash[22],hash[23],hash[24],hash[25],hash[26],hash[27],hash[28],hash[29],hash[30],
+             hash[31]); */
 }
 
 int main()
@@ -62,7 +70,7 @@ int main()
     gettimeofday(&tv1, NULL);
 
     for (i=0; i < 10000000; i++) {
-        vcalc_sha256(hashstr, hash, (uint8_t *) data, len);
+        vcalc_sha256_hardware(hashstr, hash, (uint8_t *) data, len);
     }
 
     gettimeofday(&tv2, NULL);
